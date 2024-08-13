ef7c3f0bdb4b19494c51aae7bc46f1641b994f2d
Gleiciane Barros Borges <gleiciane6217@DIGITALCOLLEGE.LOCAL>
2024-08-12 21:47:08 -0300

    primeira versao

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const PORT = 3000;
+
+const requestHandler = (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if (req.url === '/api' && req.method === 'GET') {
+        const response = { message: 'Hello,World!' }
+
+        res.writeHead(200);
+        res.end(JSON.stringify(response));
+
+    } else if (req.url === "/api" && req.method === "POST") {
+        let body = '';
+
+        req.on ('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on ('end', () => {
+            const data = JSON.parse(body);
+
+                const  response = {
+                    message: 'Data received successfully',
+                    data: data,
+                    timestamp: new Date().toISOString()
+                };
+
+                res.writeHeard(200);
+                res.end(JSON.stringify(response));
+            }
+        );
+
+    } else {
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: 'Not found' }));
+
+    }
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(PORT, () => {
+    console.log(`server is running on http://localhost:${PORT}`);
+});
